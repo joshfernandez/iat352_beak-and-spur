ac39a0b8aec4680818b82768c69d0e35ab87b5d0
Josh Fernandez <jamiguelf396@gmail.com>
2020-10-03 17:05:01 -0700

    PA1: Initialized registration page
    Includes name (first and last), email address, password, and a "Register" button

---
+++ registration.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Registration page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+
+    <h1>Register for Beak and Spur</h1>
+
+    <form>
+
+      <p>Name:
+        <input type="text" name="name" size="50" maxlength="30" />
+      </p>
+
+      <p>Email address:
+        <input type="text" name="email-address" size="50" maxlength="30" />
+      </p>
+
+      <p>Password:
+        <input type="password" name="password" size="20" maxlength="20" />
+      </p>
+
+      <input type="submit" name="register" value="Register" />
+
+    </form>
+
+  </body>
+</html>
