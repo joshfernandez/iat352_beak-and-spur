afb5782e0676ca648f41af104ac4c7c9d7fc0d61
Ethan <ethanmahc@gmail.com>
2020-10-07 17:29:58 -0700

    PA1: Implemented Grid System & Normalize.Css
    Mostly css changes

---
+++ font-family-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Registration page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- linking css sheet  -->
+    <link href="css/normalize.css" media="screen" rel="stylesheet" type="text/css"/>
+    <link href="css/font-family-page.css" media="screen" rel="stylesheet" type="text/css" />
+
+  </head>
+  <body>
+    <div id="container">
+    <H1>fasd</H1>
+    <h1>afsd</h1>
+    </div>
+
+    <h1>this works</h1>
+
+  </body>
+</html>
