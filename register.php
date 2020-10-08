02aea67e3958303a9d2fc49a82705c64c934e185
Josh Fernandez <jamiguelf396@gmail.com>
2020-10-07 22:17:57 -0700

    PA1: Introduced a separate CSS for the form pages

--- register.php
+++ register.php
@@ -5,6 +5,8 @@
     <title>Registration page</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/styles-login-and-reg-pages.css">
   </head>
 
   <body>
