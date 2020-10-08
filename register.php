ea8d2d19438eddc71f3fbbf34a8d4a3ff11dc99f
Josh Fernandez <jamiguelf396@gmail.com>
2020-10-08 13:20:50 -0700

    PA1: Improved registration page design

--- register.php
+++ register.php
@@ -17,15 +17,15 @@
 
       <form action="register-complete.php" method="post">
 
-        <p>Username:
+        <p>Username
           <input type="text" name="username" size="50" maxlength="30" />
         </p>
 
-        <p>Email address:
+        <p>Email address
           <input type="text" name="email-address" size="50" maxlength="30" />
         </p>
 
-        <p>Password:
+        <p>Password
           <input type="password" name="password" size="20" maxlength="20" />
         </p>
 
