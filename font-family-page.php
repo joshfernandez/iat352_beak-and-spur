c061f52f2cfa6e846ef1f27d38360aefeeb815cc
Ethan <ethanmahc@gmail.com>
2020-10-08 23:05:45 -0700

    PA1: CSS edits in the Font-Family Page

--- font-family-page.php
+++ font-family-page.php
@@ -1,22 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Registration page</title>
+
+<head>
+    <title>Font-Family Page</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!-- linking css sheet  -->
-    <link href="css/normalize.css" media="screen" rel="stylesheet" type="text/css"/>
+    <link href="css/normalize.css" media="screen" rel="stylesheet" type="text/css" />
     <link href="css/font-family-page.css" media="screen" rel="stylesheet" type="text/css" />
 
-  </head>
-  <body>
-    <div id="container">
-    <H1>fasd</H1>
-    <h1>afsd</h1>
+</head>
+
+<body>
+    <div class="container">
+
+        <div id="title-container">
+            <h5>December 14th, 2018</h5>
+            <h3>IBM Plex Sans</h3>
+        </div>
+
+        <div id="main-container">
+            <div class="tab">
+                <div>
+                    <button>
+                        <h5>General</h5>
+                    </button>
+                    <button>
+                        <h5>Version History</h5>
+                    </button>
+                    <button>
+                        <h5>Branches</h5>
+                    </button>
+                    <button>
+                        <h5>Examples of Work</h5>
+                    </button>
+                </div>
+                <h6>Version 3.1</h6>
+            </div>
+
+            <div class="font">
+                <form action="">
+                    <input type="text" name="text" placeholder="IBM Plex Sans">
+                </form>
+            </div>
+
+            <div class="sizing-attributes">
+                sizing shit
+            </div>
+        </div>
+
+
+        <div id="attributes-container">
+            <div class="designers-row">
+                <h4>Designers</h4>
+                <div class="border-top">
+                    Mike
+                </div>
+            </div>
+
+            <div class="tags-row">
+                <h4>Tags</h4>
+                <div class="border-top">
+                    tags
+                </div>
+            </div>
+
+            <div class="contributors-row">
+                <h4>Contributors</h4>
+                <div class="border-top">
+                    contributors
+                </div>
+            </div>
+
+            <div>
+                <button>Download</button>
+            </div>
+        </div>
+
     </div>
 
-    <h1>this works</h1>
 
-  </body>
+</body>
+
 </html>
