2e51fc6ad0520100fe96a9c60c9b552fc631c08c
Ali <brucwayn53@gmail.com>
2020-10-09 14:51:49 -0700

    added stylus + main.js
    and grunt file modification

---
+++ filter.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <title>Explore</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="Explore fonts on Beak & Spur: a community for exploring, sharing and remixing open source typography">
+   <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
+
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <!-- <meta name="msapplication-TileColor" content="#ffffff">
+   <meta name="theme-color" content="#FF2079">
+   <link rel="manifest" href="imgs/icons/manifest.json"> -->
+   <meta name="p:domain_verify" content="08e8999c41c6d49126256ccafec71782" />
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <link rel="stylesheet" href="css/filter.css">
+
+
+</head>
+
+<body>
+   <header>
+
+      <nav>
+         <div class="logo-holder"></div>
+         <input type="text" placeholder="Search..">
+
+
+
+         <div class="dropdown">
+            <img src="https://img.icons8.com/material-sharp/64/000000/person-male.png" />
+            <button class="dropbtn">˅</button>
+            <div class="dropdown-content">
+               <a href="#">Preferences</a>
+               <a href="#">Your Fonts</a>
+               <!-- <a href="#">Link 3</a> -->
+            </div>
+         </div>
+
+
+      </nav>
+   </header>
+
+   <main>
+      <h1>Explore & Remix</h1>
+      <div class="font-size-cont">
+
+         <label>
+            Font Weight
+         </label>
+         <input type="range" min="100" max="900" value="4" class="w-slider" id="myRange" step="100">
+      </div>
+
+      <section class="content-cont">
+
+
+         <article class="c-block">
+
+            <div class="c-block-settings">
+
+               <div class="c-block-settings__weight">
+                  <label>
+                     Font Weight
+                  </label>
+                  <input type="range" min="100" max="900" value="4" class="w-slider" id="myRange" step="100">
+               </div>
+               <div class="c-block-settings__italic">
+                  <label>
+                     Italics
+                  </label>
+                  <label class="switch">
+                     <input type="checkbox" checked>
+                     <span class="i-slider round"></span>
+                  </label>
+               </div>
+
+            </div>
+            <div class="c-block-tester">
+               <input type="text" name="c-block-tester__textarea" id="" placeholder="Sphinx of black quartz, judge my vow">
+            </div>
+            <div class="c-block-info">
+               <a class="c-block-info__headings">
+
+                  <h3> IBM Plex Sans
+
+                  </h3>
+                  <h4>
+                     12 Styles
+                  </h4>
+               </a>
+               <div class="c-block-info__meta">
+                  <h6 class="status">
+                     Original
+                  </h6>
+                  <div class="tags-cont">
+                     <button>sans-serif</button>
+                     <button>grotesque</button>
+                     <button>ibm</button>
+                  </div>
+               </div>
+            </div>
+
+
+         </article>
+
+      </section>
+
+      <aside class="filter-s-bar">
+         <label for="cars">Choose a car:</label>
+         <select name="cars" id="cars">
+            <optgroup label="Swedish Cars">
+               <option value="volvo">Volvo</option>
+               <option value="saab">Saab</option>
+            </optgroup>
+            <optgroup label="German Cars">
+               <option value="mercedes">Mercedes</option>
+               <option value="audi">Audi</option>
+            </optgroup>
+         </select>
+         <br><br>
+         <input type="submit" value="Submit">
+         </form>
+
+
+
+      </aside>
+
+   </main>
+
+</body>
+
+</html>
