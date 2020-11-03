d80bdd5f00f0ba449cc8eb8c394ca5cf4e1f9250
Ali <brucwayn53@gmail.com>
2020-11-02 21:17:26 -0800

    grunt enabled

--- build/register.php
+++ build/register.php
@@ -5,29 +5,32 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login page</title>
+    <title>Registration page</title>
 
     <link rel="stylesheet" href="css/styles-login-and-reg-pages.css">
   </head>
 
   <body>
 
-    <div class="login-page">
+    <div class="register-page">
 
-      <h1>Login to Beak and Spur</h1>
+      <h1>Register for Beak and Spur</h1>
 
-      <!-- <form action="login-successful.php" method="post"> -->
-      <form>
+      <form action="register-complete.php" method="post">
 
         <p>Username
           <input type="text" name="username" size="50" maxlength="30" />
         </p>
 
+        <p>Email address
+          <input type="text" name="email-address" size="50" maxlength="30" />
+        </p>
+
         <p>Password
           <input type="password" name="password" size="20" maxlength="20" />
         </p>
- 
-        <input type="submit" name="login" value="Login" />
+
+        <input type="submit" name="register" value="Register" />
 
       </form>
 
