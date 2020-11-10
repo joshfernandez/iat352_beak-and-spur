a8b604e121f2c8f07bf7ff643c044ac5bb9dd06e
Ethan <ethanmahc@gmail.com>
2020-11-09 19:07:59 -0800

    PA2: Updated Styl, Fonts, File Structure

---
+++ filter-2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Explore</title>
+    <!-- details regarding the file -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description"
+        content="Explore fonts on Beak & Spur: a community for exploring, sharing and remixing open source typography">
+    <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="p:domain_verify" content="08e8999c41c6d49126256ccafec71782" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- loading fonts into the page -->
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+
+</body>
+
+</html>
