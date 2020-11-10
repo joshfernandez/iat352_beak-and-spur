b73644df9b158207b3e0f0a58ff9e4d7b4c09ff5
Josh Fernandez <jamiguelf396@gmail.com>
2020-11-10 15:51:40 -0800

    PA2: Improve form field initialization
    Can now trim spaces and add control characters

--- php-backend/helpers/form-analysis-methods.php
+++ php-backend/helpers/form-analysis-methods.php
@@ -10,7 +10,22 @@
 <?php
 
 function initializeField($form_field) {
-  return !empty($form_field) ? $form_field : "";
+
+  if (!empty($form_field)) {
+
+    // Source: Helmine's world_db_samplecode
+    $final_value = trim($form_field);
+    if(!get_magic_quotes_gpc()){
+  		$final_value = addslashes($final_value);
+  	}
+
+    return $final_value;
+
+  }
+  else {
+    return "";
+  }
+
 }
 
 ?>
