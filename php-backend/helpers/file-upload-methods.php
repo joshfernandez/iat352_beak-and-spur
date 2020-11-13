4b49a6ad2a5af414ed5673478f730d59e3662511
Josh Fernandez <jamiguelf396@gmail.com>
2020-11-13 15:33:27 -0800

    PA2: Polished the code for image uploading

--- php-backend/helpers/file-upload-methods.php
+++ php-backend/helpers/file-upload-methods.php
@@ -19,56 +19,56 @@ function uploadImage($image_file) {
   $file_type = $image_file['type'];
 
   // Source: https://www.w3schools.com/php/php_file_upload.asp
-  $target_dir = "C:\\xampp\\htdocs\\jafernan\\user-assets\\";
+  $target_dir = "C:\\xampp\\htdocs\\josh_fernandez\\user-assets\\";
   $target_file = $target_dir . basename($file_name);
   $uploadOk = 1;
   $file_ext = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-  echo "<p>" . $file_name . " " . $file_size . " " . $file_tmp . " " . $file_type . " " . $file_ext . "</p>";
+  // echo "<p>" . $file_name . " " . $file_size . " " . $file_tmp . " " . $file_type . " " . $file_ext . "</p>";
 
   // Check if image file is a actual image or fake image
   if(isFormSubmitted($_POST["update-profile"])) {
     $check = getimagesize($file_tmp);
     if($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
+      // echo "File is an image - " . $check["mime"] . ".";
       $uploadOk = 1;
     } else {
-      echo "File is not an image.";
+      echo "<p>File is not an image.</p>";
       $uploadOk = 0;
     }
   }
 
   // Check if file already exists
   if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "<p>Sorry, the image file already exists.</p>";
     $uploadOk = 0;
   }
 
   // Check file size
   if ($file_size > 500000) {
-    echo "Sorry, your file is too large.";
+    echo "<p>Sorry, your image file is too large.</p>";
     $uploadOk = 0;
   }
 
   // Allow certain file formats
   if($file_ext != "jpg" && $file_ext != "png" && $file_ext != "jpeg"
   && $file_ext != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    echo "<p>Sorry, only JPG, JPEG, PNG & GIF files are allowed.</p>";
     $uploadOk = 0;
   }
 
   // Check if $uploadOk is set to 0 by an error
   if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo "<p>Sorry, your image file was not uploaded.</p>";
   // if everything is ok, try to upload file
   } else {
 
-    echo "<p>" . $file_tmp . " " . $target_file . "</p>";
+    // echo "<p>" . $file_tmp . " " . $target_file . "</p>";
 
     if (move_uploaded_file($file_tmp, $target_file)) {
-      echo "The file ". htmlspecialchars( basename( $image_file["name"])). " has been uploaded.";
+      echo "<p>The image file ". htmlspecialchars( basename( $image_file["name"])). " has been uploaded.</p>";
     } else {
-      echo "Sorry, there was an error uploading your file.";
+      echo "<p>Sorry, there was an error uploading your file.</p>";
     }
   }
 
