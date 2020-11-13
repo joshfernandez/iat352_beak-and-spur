8bc63579fd980caaa4edaf3b7b9766e2e4d3d2d0
Josh Fernandez <jamiguelf396@gmail.com>
2020-11-13 13:57:01 -0800

    PA2: Completed image upload for updating profile
    I just needed to use the full directory's name.

--- php-backend/helpers/file-upload-methods.php
+++ php-backend/helpers/file-upload-methods.php
@@ -11,15 +11,24 @@
 
 function uploadImage($image_file) {
 
+  // Initializing variables
+  // Source: https://www.tutorialspoint.com/php/php_file_uploading.htm
+  $file_name = $image_file['name'];
+  $file_size = $image_file['size'];
+  $file_tmp = $image_file['tmp_name'];
+  $file_type = $image_file['type'];
+
   // Source: https://www.w3schools.com/php/php_file_upload.asp
-  $target_dir = "image-uploads/";
-  $target_file = $target_dir . basename($image_file["name"]);
+  $target_dir = "C:\\xampp\\htdocs\\jafernan\\user-assets\\";
+  $target_file = $target_dir . basename($file_name);
   $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  $file_ext = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+  echo "<p>" . $file_name . " " . $file_size . " " . $file_tmp . " " . $file_type . " " . $file_ext . "</p>";
 
   // Check if image file is a actual image or fake image
   if(isFormSubmitted($_POST["update-profile"])) {
-    $check = getimagesize($image_file["tmp_name"]);
+    $check = getimagesize($file_tmp);
     if($check !== false) {
       echo "File is an image - " . $check["mime"] . ".";
       $uploadOk = 1;
@@ -36,14 +45,14 @@ function uploadImage($image_file) {
   }
 
   // Check file size
-  if ($image_file["size"] > 500000) {
+  if ($file_size > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
   }
 
   // Allow certain file formats
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "gif" ) {
+  if($file_ext != "jpg" && $file_ext != "png" && $file_ext != "jpeg"
+  && $file_ext != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
   }
@@ -53,7 +62,10 @@ function uploadImage($image_file) {
     echo "Sorry, your file was not uploaded.";
   // if everything is ok, try to upload file
   } else {
-    if (move_uploaded_file($_FILES["profile-image"]["tmp_name"], $target_file)) {
+
+    echo "<p>" . $file_tmp . " " . $target_file . "</p>";
+
+    if (move_uploaded_file($file_tmp, $target_file)) {
       echo "The file ". htmlspecialchars( basename( $image_file["name"])). " has been uploaded.";
     } else {
       echo "Sorry, there was an error uploading your file.";
