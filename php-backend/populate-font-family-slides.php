d6c907d2152e2ea61b049f2a9054ebad11df6020
Josh Fernandez <jamiguelf396@gmail.com>
2020-12-04 09:46:20 -0800

    PA3: Solve the issue with no logged-in user
    Two functions: populateSlidesForVisitor() and populateSlidesDefault()

--- php-backend/populate-font-family-slides.php
+++ php-backend/populate-font-family-slides.php
@@ -26,82 +26,184 @@
 
   // Session is already declared in set_header.php.
 
-  // 2 - Open a connection to the josh_fenandez database.
-  $db_connection = openDBConnection();
+  /***********
+    Helper methods
+  ***********/
+  
+  // Case A: For the visitor
+  function populateSlidesForVisitor($logged_user) {
 
-  // 3 - SELECT query to access full rows in the database
-  $font_families_result = $db_connection -> query("SELECT * FROM font_families;");
+    // 2 - Open a connection to the josh_fenandez database.
+    $db_connection = openDBConnection();
 
-  if(!$font_families_result) {
-    die("Database query failed.");
-  }
+    // 3 - SELECT query to access full rows in the database
+    $font_families_result = $db_connection -> query("SELECT * FROM font_families;");
 
-  // 4A - Write the SELECT SQL query for the user's favourite font types.
-  $fav_types_query = "SELECT members.favourite_font_types FROM members WHERE members.username = '" . $logged_user ."';";
-
-  $fav_types_result = $db_connection->query($fav_types_query);
-
-  if(!$fav_types_result) {
-    die("Database query failed.");
-  }
-
-  // 4B - Extract the list of favourite types.
-  $fav_types_list_str = mysqli_fetch_row($fav_types_result)[0];
-  $fav_types_list = explode(",", $fav_types_list_str);
-
-  // 4 - Populate the explore page with slides.
-  // Copied from populate-font-type-checkboxes.php
-  while ($font_family_arr = mysqli_fetch_assoc($font_families_result)) {
-
-    // First, retrieve font family's tags.
-    $tags_display = $font_family_arr['font_type'];
-    // Assign Associative Array into a variable
-    $tagsArray = (explode(",", $tags_display));
-
-    // Only print if it is a suggested font family.
-    if(isSuggestedFontFamily($fav_types_list, $tagsArray)) {
-
-      // Get size of Assoc Array
-      $tagsArraySize = sizeof($tagsArray);
-
-      // retrieve font name, designer name, tags from database
-      $family_id_display = $font_family_arr['family_id'];
-      $family_name_display = $font_family_arr['family_name'];
-      $designer_name_display = $font_family_arr['designer'];
-
-      echo "<div class=\"explore-slide\">";
-
-      // display designer name & font-family name
-      echo "<h6 class='margin-bottom-lv1'></h6>" .
-      "<h5>Designed by <span class='italic'>$designer_name_display</span></h5>" .
-      "<h1 style='font-size: 7em; font-family:LeagueGothic'>$family_name_display</h1>";
-
-      echo "<div class=\"explore-font-tags-container\">";
-
-      // run a for loop to dynamically generate all tags
-      for ($x = 0; $x < $tagsArraySize; $x++) {
-          // grab assoc array values and put in variables
-          $tagsPrint = $tagsArray[$x];
-          echo "<div class='explore-font-tags'>" .
-          "<img src='../assets/img/tag-lines.png' alt='no tag lines'>" .
-          "<h6>$tagsPrint</h6>" .
-          "</div> ";
-      }
-
-      echo "</div>"; // Closes explore-font-tags-container
-
-      echo "<a href=\"font-family-page.php?varname=" . $family_id_display . "\">View Font Family</a>";
-
-      echo "</div>"; // Closes explore-slide
-
+    if(!$font_families_result) {
+      die("Database query failed.");
     }
 
-  };
+    // 4A - Write the SELECT SQL query for the user's favourite font types.
+    $fav_types_query = "SELECT members.favourite_font_types FROM members WHERE members.username = '" . $logged_user ."';";
 
-  // 5 - Release returned data.
-  mysqli_free_result($font_families_result);
+    $fav_types_result = $db_connection->query($fav_types_query);
 
-  // 6 - Close the database connection.
-  mysqli_close($db_connection);
+    if(!$fav_types_result) {
+      die("Database query failed.");
+    }
+
+    // 4B - Extract the list of favourite types.
+    $fav_types_list_str = mysqli_fetch_row($fav_types_result)[0];
+    $fav_types_list = explode(",", $fav_types_list_str);
+
+    // 4 - Populate the explore page with slides.
+    // Copied from populate-font-type-checkboxes.php
+    while ($font_family_arr = mysqli_fetch_assoc($font_families_result)) {
+
+      // First, retrieve font family's tags.
+      $tags_display = $font_family_arr['font_type'];
+      // Assign Associative Array into a variable
+      $tagsArray = (explode(",", $tags_display));
+
+      // Only print if it is a suggested font family.
+      if(isSuggestedFontFamily($fav_types_list, $tagsArray)) {
+
+        // Get size of Assoc Array
+        $tagsArraySize = sizeof($tagsArray);
+
+        // retrieve font name, designer name, tags from database
+        $family_id_display = $font_family_arr['family_id'];
+        $family_name_display = $font_family_arr['family_name'];
+        $designer_name_display = $font_family_arr['designer'];
+
+        echo "<div class=\"explore-slide\">";
+
+        // display designer name & font-family name
+        echo "<h6 class='margin-bottom-lv1'></h6>" .
+        "<h5>Designed by <span class='italic'>$designer_name_display</span></h5>" .
+        "<h1 style='font-size: 7em; font-family:LeagueGothic'>$family_name_display</h1>";
+
+        echo "<div class=\"explore-font-tags-container\">";
+
+        // run a for loop to dynamically generate all tags
+        for ($x = 0; $x < $tagsArraySize; $x++) {
+            // grab assoc array values and put in variables
+            $tagsPrint = $tagsArray[$x];
+            echo "<div class='explore-font-tags'>" .
+            "<img src='../assets/img/tag-lines.png' alt='no tag lines'>" .
+            "<h6>$tagsPrint</h6>" .
+            "</div> ";
+        }
+
+        echo "</div>"; // Closes explore-font-tags-container
+
+        echo "<a href=\"font-family-page.php?varname=" . $family_id_display . "\">View Font Family</a>";
+
+        echo "</div>"; // Closes explore-slide
+
+      }
+
+    };
+
+    // 5 - Release returned data.
+    mysqli_free_result($font_families_result);
+    mysqli_free_result($fav_types_result);
+
+    // 6 - Close the database connection.
+    mysqli_close($db_connection);
+
+  }
+
+
+  // Case B: For the member
+  function populateSlidesDefault() {
+
+    // 2 - Open a connection to the josh_fenandez database.
+    $db_connection = openDBConnection();
+
+    // 3 - SELECT query to access full rows in the database
+    $font_families_result = $db_connection -> query("SELECT * FROM font_families;");
+
+    if(!$font_families_result) {
+      die("Database query failed.");
+    }
+
+    // // 4A - Write the SELECT SQL query for the user's favourite font types.
+    // $fav_types_query = "SELECT members.favourite_font_types FROM members WHERE members.username = '" . $logged_user ."';";
+    //
+    // $fav_types_result = $db_connection->query($fav_types_query);
+    //
+    // if(!$fav_types_result) {
+    //   die("Database query failed.");
+    // }
+    //
+    // // 4B - Extract the list of favourite types.
+    // $fav_types_list_str = mysqli_fetch_row($fav_types_result)[0];
+    // $fav_types_list = explode(",", $fav_types_list_str);
+
+    // 4 - Populate the explore page with slides.
+    // Copied from populate-font-type-checkboxes.php
+    while ($font_family_arr = mysqli_fetch_assoc($font_families_result)) {
+
+      // First, retrieve font family's tags.
+      $tags_display = $font_family_arr['font_type'];
+      // Assign Associative Array into a variable
+      $tagsArray = (explode(",", $tags_display));
+
+      // Only print if it is a suggested font family.
+      // if(isSuggestedFontFamily($fav_types_list, $tagsArray)) {
+
+        // Get size of Assoc Array
+        $tagsArraySize = sizeof($tagsArray);
+
+        // retrieve font name, designer name, tags from database
+        $family_id_display = $font_family_arr['family_id'];
+        $family_name_display = $font_family_arr['family_name'];
+        $designer_name_display = $font_family_arr['designer'];
+
+        echo "<div class=\"explore-slide\">";
+
+        // display designer name & font-family name
+        echo "<h6 class='margin-bottom-lv1'></h6>" .
+        "<h5>Designed by <span class='italic'>$designer_name_display</span></h5>" .
+        "<h1 style='font-size: 7em; font-family:LeagueGothic'>$family_name_display</h1>";
+
+        echo "<div class=\"explore-font-tags-container\">";
+
+        // run a for loop to dynamically generate all tags
+        for ($x = 0; $x < $tagsArraySize; $x++) {
+            // grab assoc array values and put in variables
+            $tagsPrint = $tagsArray[$x];
+            echo "<div class='explore-font-tags'>" .
+            "<img src='../assets/img/tag-lines.png' alt='no tag lines'>" .
+            "<h6>$tagsPrint</h6>" .
+            "</div> ";
+        }
+
+        echo "</div>"; // Closes explore-font-tags-container
+
+        echo "<a href=\"font-family-page.php?varname=" . $family_id_display . "\">View Font Family</a>";
+
+        echo "</div>"; // Closes explore-slide
+
+      // }
+
+    };
+
+    // 5 - Release returned data.
+    mysqli_free_result($font_families_result);
+
+    // 6 - Close the database connection.
+    mysqli_close($db_connection);
+
+  }
+
+  // Back to the main procedure: Go to the corresponding code for either visitor or member.
+  if(isset($logged_user) && !empty($logged_user)) {
+    populateSlidesForVisitor($logged_user);
+  }
+  else {
+    populateSlidesDefault();
+  }
 
 ?>
--- preferences.php
+++ preferences.php
@@ -22,17 +22,18 @@
 
         <h1>Personalize your fonts and font searches</h1>
 
+
         <h2>Favourite font types</h2>
 
         <p>Choose your favourite font types from the list below.</p>
 
         <form action="preferences-complete.php" method="post">
 
-            <p>
-                <?php include "php-backend/populate-font-type-checkboxes.php"; ?>
-            </p>
+          <p>
+            <?php include "php-backend/populate-font-type-checkboxes.php"; ?>
+          </p>
 
-            <input type="submit" name="update-preferences" value="Update your preferences" />
+          <input type="submit" name="update-preferences" value="Update your preferences" />
 
         </form>
 
